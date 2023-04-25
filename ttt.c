9f10c92c1742dae0bdd0affa045ddc09afde4bd6
Jack Lin <hellojjlin@gmail.com>
2023-04-25 19:19:13 -0400

    starting ttt.c / prototypes

--- ttt.c
+++ ttt.c
@@ -1,7 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, int** argv) {
-    printf("hello world");
+char board[3][3];
+const char PLAYER1 = 'X';
+const char PLAYER2 = 'O';
+
+void resetBoard();
+void printBoard();
+int checkFreeSpaces();
+void player1Move();
+void player2Move();
+char checkWinner();
+void printWinner(char);
+
+int main(int argc, char** argv) {
+/*  if (argc < 3) {
+        printf("error: Not enough arguments for ttt.c\n");
+    }
+    char* domainName = argv[1];
+    char* portNumber = argv[2];
+*/
+
+    char winner = ' '; 
+    resetBoard();
+
+
+
     return 0;
 }
+
+void resetBoard();
+void printBoard();
+int checkFreeSpaces();
+void playerMove();
+void player2Move();
+char checkWinner();
+void printWinner(char winner);
